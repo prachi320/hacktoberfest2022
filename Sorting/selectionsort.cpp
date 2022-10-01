746d2a3cd7259ec51eb73b424548da8fee78c80b
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-10-01 11:01:54 +0530

    Update selectionsort.cpp
    UPDATED SELECTION SORT

--- Sorting/selectionsort.cpp
+++ Sorting/selectionsort.cpp
@@ -1,39 +1,55 @@
-#include<iostream>
-#include<bits/stdc++.h>
-
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
 using namespace std;
-
-void display(int n,int arr[]){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";   
-    }
+ 
+//Swap function
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
-
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-
-    display(n,arr);
-
-    for (int i = 0; i < n; i++)
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of
+    // unsorted subarray
+    for (i = 0; i < n-1; i++)
     {
-        int minindex = i;
-
-        for(int j=i+1; j<n; j++){
-            if(arr[j] < arr[minindex]){
-                minindex = j;
-            }
-        }
-        
-        swap(arr[i],arr[minindex]);
+       
+        // Find the minimum element in
+        // unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element
+        // with the first element
+        if(min_idx!=i)
+            swap(&arr[min_idx], &arr[i]);
     }
-    
-    cout<<endl;
-    display(n,arr);
 }
-
-
+ 
+//Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
