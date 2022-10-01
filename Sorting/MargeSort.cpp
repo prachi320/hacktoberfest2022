848298b0c607670abe54779f8750d9c434c17e39
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-10-01 10:50:25 +0530

    Update MargeSort.cpp
    updated merge version

--- Sorting/MargeSort.cpp
+++ Sorting/MargeSort.cpp
@@ -1,92 +1,69 @@
-#include <iostream>
- 
+#include<iostream>
 using namespace std;
- 
-// A function to merge the two half into a sorted data.
-void Merge(int *a, int low, int high, int mid)
-{
-	// We have low to mid and mid+1 to high already sorted.
-	int i, j, k, temp[high-low+1];
-	i = low;
-	k = 0;
-	j = mid + 1;
- 
-	// Merge the two parts into temp[].
-	while (i <= mid && j <= high)
-	{
-		if (a[i] < a[j])
-		{
-			temp[k] = a[i];
-			k++;
-			i++;
-		}
-		else
-		{
-			temp[k] = a[j];
-			k++;
-			j++;
-		}
-	}
- 
-	// Insert all the remaining values from i to mid into temp[].
-	while (i <= mid)
-	{
-		temp[k] = a[i];
-		k++;
-		i++;
-	}
- 
-	// Insert all the remaining values from j to high into temp[].
-	while (j <= high)
-	{
-		temp[k] = a[j];
-		k++;
-		j++;
-	}
- 
- 
-	// Assign sorted data stored in temp[] to a[].
-	for (i = low; i <= high; i++)
-	{
-		a[i] = temp[i-low];
-	}
+void swapping(int &a, int &b) {     //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
 }
- 
-// A function to split array into two parts.
-void MergeSort(int *a, int low, int high)
-{
-	int mid;
-	if (low < high)
-	{
-		mid=(low+high)/2;
-		// Split the data into two half.
-		MergeSort(a, low, mid);
-		MergeSort(a, mid+1, high);
- 
-		// Merge them to get sorted output.
-		Merge(a, low, high, mid);
-	}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
 }
- 
-int main()
-{
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
- 
-	int arr[n];
-	for(i = 0; i < n; i++)
-	{
-		cout<<"Enter element "<<i+1<<": ";
-		cin>>arr[i];
-	}
- 
-	MergeSort(arr, 0, n-1);
- 
-	// Printing the sorted data.
-	cout<<"\nSorted Data ";
-	for (i = 0; i < n; i++)
-        cout<<"->"<<arr[i];
- 
-	return 0;
+void merge(int *array, int l, int m, int r) {
+   int i, j, k, nl, nr;
+   //size of left and right sub-arrays
+   nl = m-l+1; nr = r-m;
+   int larr[nl], rarr[nr];
+   //fill left and right sub-arrays
+   for(i = 0; i<nl; i++)
+      larr[i] = array[l+i];
+   for(j = 0; j<nr; j++)
+      rarr[j] = array[m+1+j];
+   i = 0; j = 0; k = l;
+   //marge temp arrays to real array
+   while(i < nl && j<nr) {
+      if(larr[i] <= rarr[j]) {
+         array[k] = larr[i];
+         i++;
+      }else{
+         array[k] = rarr[j];
+         j++;
+      }
+      k++;
+   }
+   while(i<nl) {       //extra element in left array
+      array[k] = larr[i];
+      i++; k++;
+   }
+   while(j<nr) {     //extra element in right array
+      array[k] = rarr[j];
+      j++; k++;
+   }
+}
+void mergeSort(int *array, int l, int r) {
+   int m;
+   if(l < r) {
+      int m = l+(r-l)/2;
+      // Sort first and second arrays
+      mergeSort(array, l, m);
+      mergeSort(array, m+1, r);
+      merge(array, l, m, r);
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];     //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   mergeSort(arr, 0, n-1);     //(n-1) for last index
+   cout << "Array after Sorting: ";
+   display(arr, n);
 }
